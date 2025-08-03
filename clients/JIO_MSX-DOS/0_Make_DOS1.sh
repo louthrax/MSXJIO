6623107edff614216fc22a42ffbe147b385b7118
Louthrax <lhalter@free.fr>
2025-08-03 18:59:31 +0200

    Updates build scripts for MSX-DOS
    Refactors the build scripts for MSX-DOS 1 and 2.
    
    This change updates the build process by adding `set -ex` and `export WINEDEBUG=-all` for debugging.
    It also corrects the z88dk commands and updates the file copy destination.

--- clients/JIO_MSX-DOS/0_Make_DOS1.sh
+++ clients/JIO_MSX-DOS/0_Make_DOS1.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -ex
+
+export WINEDEBUG=-all
+
 cd "$(dirname "$0")"
 
 wine iccZ80.exe drv_jio.c -z9 -uu -a drv_jio_c.as
@@ -7,16 +11,16 @@ wine iccZ80.exe drv_jio.c -z9 -uu -a drv_jio_c.as
 
 rm -r -f ./obj rdate.inc
 date +"db \"%Y-%m-%d\"" > rdate.inc
-z88dk.z88dk-z80asm -b -d -l -m -DJIO -DIDEDOS1 -Oobj -o=jio_dos1.bin dos1x.asm drv_jio.asm
-z88dk.z88dk-appmake +glue -b obj/jio_dos1 --filler 0xFF --clean
+z88dk-z80asm -b -d -l -m -DJIO -DIDEDOS1 -Oobj -o=jio_dos1.bin dos1x.asm drv_jio.asm
+z88dk-appmake +glue -b obj/jio_dos1 --filler 0xFF --clean
 
-z88dk.z88dk-appmake +rom  -b obj/jio_dos1__.bin -o ./jio_dos1.rom -s 16384 --org 0
+z88dk-appmake +rom  -b obj/jio_dos1__.bin -o ./jio_dos1.rom -s 16384 --org 0
 
-z88dk.z88dk-appmake +rom  -b obj/jio_dos1__.bin -o ./jio_dos1_64k.rom -s 65536 --org 16384 --fill 0xFF
+z88dk-appmake +rom  -b obj/jio_dos1__.bin -o ./jio_dos1_64k.rom -s 65536 --org 16384 --fill 0xFF
 
 dd if=/dev/zero bs=1 count=65536 | tr '\0' '\377' > jio_dos1_64k_NMS_8220.rom
 dd if=jio_dos1.rom of=jio_dos1_64k_NMS_8220.rom bs=1 count=16384 seek=0 conv=notrunc
 dd if=jio_dos1.rom of=jio_dos1_64k_NMS_8220.rom bs=1 skip=16384 count=16384 seek=32768 conv=notrunc
 
-cp ./jio_dos1.rom /media/laurent/DataBackupNAS/msxftp/RSDISK
+cp ./jio_dos1.rom /mnt/DataBackupNAS/msxftp/RSDISK
 rm -r -f ./obj rdate.inc
--- clients/JIO_MSX-DOS/0_Make_DOS2.sh
+++ clients/JIO_MSX-DOS/0_Make_DOS2.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -ex
+
+export WINEDEBUG=-all
+
 cd "$(dirname "$0")"
 
 wine iccZ80.exe drv_jio.c -z9 -uu -a drv_jio_c.as
@@ -7,12 +11,12 @@ wine iccZ80.exe drv_jio.c -z9 -uu -a drv_jio_c.as
 
 rm -r -f ./obj rdate.inc
 date +"db \"%Y-%m-%d\"" > rdate.inc
-z88dk.z88dk-z80asm -b -d -l -m -DJIO -Oobj -o=jio_dos2.bin p1_main.asm p3_paging.asm drv_jio.asm p0_kernel.asm
-z88dk.z88dk-appmake +glue -b obj/jio_dos2 --filler 0xFF --clean
+z88dk-z80asm -b -d -l -m -DJIO -Oobj -o=jio_dos2.bin p1_main.asm p3_paging.asm drv_jio.asm p0_kernel.asm
+z88dk-appmake +glue -b obj/jio_dos2 --filler 0xFF --clean
 
-z88dk.z88dk-appmake +rom  -b obj/jio_dos2__.bin -o ./jio_dos2.rom -s 32768 --org 0
+z88dk-appmake +rom  -b obj/jio_dos2__.bin -o ./jio_dos2.rom -s 32768 --org 0
 
-z88dk.z88dk-appmake +rom  -b obj/jio_dos2__.bin -o ./jio_dos2_64k.rom -s 65536 --org 16384 --fill 0xFF
+z88dk-appmake +rom  -b obj/jio_dos2__.bin -o ./jio_dos2_64k.rom -s 65536 --org 16384 --fill 0xFF
 
 dd if=/dev/zero bs=1 count=65536 | tr '\0' '\377' > jio_dos2_64k_NMS_8220.rom
 
@@ -21,5 +25,5 @@ dd if=jio_dos2.rom of=jio_dos2_64k_NMS_8220.rom bs=1 count=16384 skip=0     seek
 dd if=jio_dos2.rom of=jio_dos2_64k_NMS_8220.rom bs=1 count=16384 skip=16384 seek=0     conv=notrunc
 dd if=jio_dos2.rom of=jio_dos2_64k_NMS_8220.rom bs=1 count=16384 skip=16384 seek=32768 conv=notrunc
 
-cp ./jio_dos2.rom /media/laurent/DataBackupNAS/msxftp/RSDISK
+cp ./jio_dos2.rom /mnt/DataBackupNAS/msxftp/RSDISK
 rm -r -f ./obj rdate.inc
